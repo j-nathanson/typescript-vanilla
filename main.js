22405e02c95a41109be963228600c2aca237d7ca
jacobnathanson <jacobnath93@gmail.com>
2022-04-15 13:15:05 -0400

    variable declarations

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ exports.__esModule = true;
 var message = 'Hello Bobby';
 // run indiv file with tsc main
 console.log(message);
+var x = 10;
+// cannot do let x = 2; already declared
+// can do x = 2;
+var y = 20;
+var sum;
+var title = 'Bobproject';
--- main.ts
+++ main.ts
@@ -2,3 +2,11 @@ export { } //use to have ts and js files open in editor at the same time
 let message = 'Hello Bobby'
 // run indiv file with tsc main
 console.log(message)
+
+let x = 10;
+// cannot do let x = 2; already declared
+// can do x = 2;
+const y = 20;
+
+let sum;
+const title = 'Bobproject';
