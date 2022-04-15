463c26a24bbe93d22b07477195493cad4d11f485
jacobnathanson <jacobnath93@gmail.com>
2022-04-15 13:10:21 -0400

    export

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-var message = 'Hello bob';
+"use strict";
+exports.__esModule = true;
+var message = 'Hello Bobby';
 // run indiv file with tsc main
 console.log(message);
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
-let message = 'Hello bob'
+export { } //use to have ts and js files open in editor at the same time
+let message = 'Hello Bobby'
 // run indiv file with tsc main
 console.log(message)
